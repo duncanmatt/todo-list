77c948b0c33797d5724faa64088c59bb01016770
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2022-11-11 00:01:26 -0500

    clarity

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import "./styles.css";
 const container = document.getElementById('container');
 const projects = document.querySelectorAll('.proj');
 const greeting = document.getElementById('greeting');
+const lists = document.querySelectorAll('.task-list');
+const list1 = document.getElementById('proj1-list');
+const list2 = document.getElementById('proj2-list');
+const list3 = document.getElementById('proj3-list');
 
 function getDate() {
   let today = new Date();
@@ -23,5 +27,15 @@ getDate();
 
 
 // --logic--
+class Task {
+  constructor(title, desc, deadline, priority, duration) {
+    this.title = title;
+    this.desc = desc;
+    this.deadline = deadline;
+    this.priority = priority;
+    this.duration = duration;
+  }
+}
 
-
+const test = new Task('hello', 'dsjhaa', 'adshjfhk', 'aljlak', 'jad');
+console.log(test);
