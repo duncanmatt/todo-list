04ab510796a424cb839ab4db025aae1d0ce661d7
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2022-11-11 22:04:03 -0500

    creates new list item on form submission

--- src/index.js
+++ src/index.js
@@ -5,10 +5,18 @@ const greeting = document.getElementById('greeting');
 
 const proj1Form = document.getElementById('proj1-form');
 const proj1List = document.getElementById('proj1-list');
+const titleInput1 = document.getElementById('proj1-title-input');
+const infoInput1 = document.getElementById('proj1-info-input');
 let proj1Tasks = [];
 
 // localStorage.setItem(new Task(title.value, desc.value);
 // --logic--
+class Task {
+  constructor(title, info) {
+    this.title = title;
+    this.info = info;
+  }
+}
 
 addTask();
 function addTask() {
@@ -17,23 +25,18 @@ function addTask() {
     const task = new FormData(e.target);
     const newTask = Object.fromEntries(task.entries());
     proj1Tasks.push(newTask);
-    console.log(proj1Tasks);
+    
+    const newItem = document.createElement('li');
+    newItem.innerHTML = `<p>${titleInput1.value}<br>${infoInput1.value}</p>`;
+    proj1List.appendChild(newItem);
     proj1Form.reset();
-    createList();
   }
   
 }
 
 function createList() {
-  for(let task of proj1Tasks) {
-    for(let prop of task) {
-      const newItem = document.createElement('li');
-      proj1List.appendChild(newItem)
+  for(const item of proj1Tasks) {
     
-      const itemInfo = document.createElement('p');
-      itemInfo.textContent = prop;
-      newItem.appendChild(itemInfo);
-    }
   }
 }
 
