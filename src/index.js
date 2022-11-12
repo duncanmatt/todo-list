9b45379ad45340c0706d796f9b63eabc7c65d847
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2022-11-12 01:07:53 -0500

    UI update

--- src/index.js
+++ src/index.js
@@ -7,17 +7,118 @@ const proj1Form = document.getElementById('proj1-form');
 const proj1List = document.getElementById('proj1-list');
 const titleInput1 = document.getElementById('proj1-title-input');
 const infoInput1 = document.getElementById('proj1-info-input');
-let proj1Tasks = [];
 
-// localStorage.setItem(new Task(title.value, desc.value);
-// --logic--
-class Task {
-  constructor(title, info) {
-    this.title = title;
-    this.info = info;
-  }
+let proj1Tasks = [
+  'create two more projects',
+  'improve UI/UX',
+  'figure out hosting',
+];
+const proj1TaskItems = [];
+
+let dragStartIndex;
+
+createTasks();
+function createTasks() {
+  [...proj1Tasks]
+    .forEach((task, index) => {
+      const listItem = document.createElement('li');
+
+      listItem.setAttribute('data-priority', index);
+
+      listItem.innerHTML = `
+        <span class="number">Priority: ${index+1}</span>
+        <div class="draggable" draggable="true">
+          <p class="task-name">${task}</p>
+          <i class="fa-solid fa-trash"></i>
+        </div>
+      `;
+
+      proj1TaskItems.push(listItem);
+
+      proj1List.appendChild(listItem);
+    });
+
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --logic--
+
+/*
 addTask();
 function addTask() {
   proj1Form.onsubmit = (e) => {
@@ -33,12 +134,7 @@ function addTask() {
   }
   
 }
-
-function createList() {
-  for(const item of proj1Tasks) {
-    
-  }
-}
+*/
 
 // --DOM--
 function getDate() {
