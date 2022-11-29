3cfb115797101e430210dceabafdf3ace2b5c54c
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2022-11-28 19:36:52 -0500

    storage and tasks centered

--- src/index.js
+++ src/index.js
@@ -2,17 +2,91 @@ import _ from "lodash";
 import "./styles.css";
 
 class Task {
-  constructor(title, info) {
-    this.title = title;
-    this.info = info;
+  constructor(key, val) {
+    this.key = key;
+    this.val = val
   }
-  // store()
+  toString() {
+    return '[' + this.key + ' - ' + this.val + ']';
+  }
 }
 
+class TaskTable {
+  constructor(size=53) {
+    this.keyMap = new Array(size);
+  }
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) + 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  set(key, val) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, val]);
+  }
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+}
+let taskStorage = new TaskTable();
+
 window.addEventListener('load', () => {
+  updateStorage();
   createTaskItems();
 });
+// push every key and value from local storage into datastruct
+function updateStorage() {
+  for (const [key, value] of Object.entries(localStorage)) {
+    taskStorage.set(`${key}`, JSON.parse(value));
+  }
+  console.log(taskStorage);
+  
+}
 
+
+// adding tasks to localstorage
 const taskForm = document.getElementById('task-form');
 taskForm.onsubmit = (e) => {
   e.preventDefault();
@@ -20,43 +94,73 @@ taskForm.onsubmit = (e) => {
 }
 
 function addTask() {
-  const taskTitle = document.getElementById('task-title-input');
-  const taskInfo = document.getElementById('task-info-input');
-  let newTask = new Task(taskTitle.value, taskInfo.value);
-
+  const taskKey = document.getElementById('task-key-input');
+  const taskVal = document.getElementById('task-val-input');
+  
   if (document.getElementById('pw-select').checked) {
-    localStorage.setItem(newTask.title, JSON.stringify(newTask.info));
+    localStorage.setItem(taskKey.value, JSON.stringify(taskVal.value));
+    createItem(taskKey.value, taskVal.value);
     taskForm.reset();
   } 
   else if(document.getElementById('ff-select').checked) {
-    localStorage.setItem(newTask.title, JSON.stringify(newTask.info));
+    localStorage.setItem(taskKey.value, JSON.stringify(taskVal.value));
+    createItem(taskKey.value, taskVal.value);
     taskForm.reset();
   }
   else if(document.getElementById('ttt-select').checked) {
-    localStorage.setItem(newTask.title, JSON.stringify(newTask.info));
+    localStorage.setItem(taskKey.value, JSON.stringify(taskVal.value));
+    createItem(taskKey.value, taskVal.value);
     taskForm.reset();
   } else {
     formFeedback();
     console.log('TASK DID NOT SUBMIT');
   }
+  updateStorage();
+}
+function createItem(key, val) {
+  const taskList = document.querySelector('.task-list');
+  const newItem = document.createElement('li');
+    newItem.innerHTML = `<div class="wrapper">
+                          <div class="item-top">
+                            <h4>${key}</h4>
+                            <span class="del">
+                              <i class="fa-solid fa-trash"></i>
+                            </span>
+                          </div>
+                          <div class="item-bottom">
+                            <h6>${val}</h6>
+                          </div>
+                        </div>
+                        `;
+    newItem.classList.add('item');
+    taskList.appendChild(newItem); 
 }
 
+// create ui items from storage list
 function createTaskItems() {
   const taskList = document.querySelector('.task-list');
-  for (let i = 0; i < localStorage.length; i++) {
-    let taskKey = localStorage.key(i);
-    let taskContent = JSON.parse(
-      localStorage.getItem(taskKey));
-    let newItem = document.createElement('li');
-    newItem.innerHTML = `
-                          <h4>${taskKey}</h4>
-                          <h6>${taskContent}</h6>
+  let storage = taskStorage.keys();
+  for (const key of storage) {
+    const newTask = new Task(key, taskStorage.get(key));
+    const newItem = document.createElement('li');
+    newItem.innerHTML = `<div class="wrapper">
+                          <div class="item-top">
+                            <h4>${newTask.key}</h4>
+                            <span class="del">
+                              <i class="fa-solid fa-trash"></i>
+                            </span>
+                          </div>
+                          <div class="item-bottom">
+                            <h6>${newTask.val}</h6>
+                          </div>
+                        </div>
                         `;
-    taskList.appendChild(newItem);
     newItem.classList.add('item');
+    taskList.appendChild(newItem); 
   }
-}
+}    
 
+// display the current date at the top 
 function getDate() {
   const greeting = document.getElementById('greeting');
   let today = new Date();
@@ -69,8 +173,6 @@ function getDate() {
 getDate();
 
 // if project input is selected: illuminate corresponding project with border-box
-const projSelectors = document.querySelectorAll('[name=project]');
-
 function formFeedback() {
   if (document.getElementById('pw-select').checked) {
     proj1.style.boxShadow = '0 10px 20px var(--lighter), 0 6px 6px var(--mid)';
@@ -94,13 +196,17 @@ function formFeedback() {
   }
 }
 
+const projSelectors = document.querySelectorAll('[name=project]');
 projSelectors.forEach((selector) => {
   selector.addEventListener('click', () => {
     formFeedback();
   });
 });
 
-//--Logic--
+// click to remove task
+
+
+
 
 
 
